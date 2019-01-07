fbb5b4bc5c1aab3b0939dd4c87edd67f53357f86
Marian Stanciulica <marian.stanciulica@gmail.com>
2019-01-07 21:34:41 +0200

    Ship Movement Added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float speed = 15f;
+    [SerializeField] float xRange = 8f;
+    [SerializeField] float yRange = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+        float rawYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
+
+        transform.localPosition = new Vector3(rawXPos, rawYPos, transform.localPosition.z);
+
+	}
+}

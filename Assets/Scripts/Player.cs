e112ed2a8fa9a5067e9de8eca840a894a14182da
Marian Stanciulica <marian.stanciulica@gmail.com>
2019-01-07 21:54:30 +0200

    Ship Rotation Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,15 +9,37 @@ public class Player : MonoBehaviour {
     [SerializeField] float xRange = 8f;
     [SerializeField] float yRange = 5f;
 
+    [SerializeField] float positionPitchFactor =   -5f;
+    [SerializeField] float controlPitchFactor =   -20f;
+    [SerializeField] float positionYawFactor =      5f;
+    [SerializeField] float controlRollFactor =    -20f;
+
+    float xThrow = 0;
+    float yThrow = 0;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+    private void ProcessRotation()
+    {
+        float pitch = transform.localRotation.y * positionPitchFactor + yThrow * positionPitchFactor;
+        float yaw = transform.localRotation.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * speed * Time.deltaTime;
         float yOffset = yThrow * speed * Time.deltaTime;
@@ -26,6 +48,5 @@ public class Player : MonoBehaviour {
         float rawYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
 
         transform.localPosition = new Vector3(rawXPos, rawYPos, transform.localPosition.z);
-
-	}
+    }
 }
